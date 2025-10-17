16a5bc7f538de9add7fd2f214235ba0783c5c6d2
Vladimir <vlad.stavros@bk.ru>
2025-10-17 08:32:24 +0300

    Отображение предложений на карте

--- src/mocks/offers.ts
+++ src/mocks/offers.ts
@@ -8,16 +8,16 @@ export const offers: Offer[] = [
     price: 505,
     previewImage: 'https://15.design.htmlacademy.pro/static/hotel/15.jpg',
     city: {
-      name: 'Paris',
+      name: 'Amsterdam',
       location: {
-        latitude: 48.85661,
-        longitude: 2.351499,
+        latitude: 52.3609553943508,
+        longitude: 4.85309666406198,
         zoom: 13,
       },
     },
     location: {
-      latitude: 48.868610000000004,
-      longitude: 2.342499,
+      latitude: 52.3909553943508,
+      longitude: 4.85309666406198,
       zoom: 16,
     },
     isFavorite: false,
@@ -31,16 +31,16 @@ export const offers: Offer[] = [
     price: 957,
     previewImage: 'https://15.design.htmlacademy.pro/static/hotel/20.jpg',
     city: {
-      name: 'Paris',
+      name: 'Amsterdam',
       location: {
-        latitude: 48.85661,
-        longitude: 2.351499,
+        latitude: 52.3609553943508,
+        longitude: 4.85309666406198,
         zoom: 13,
       },
     },
     location: {
-      latitude: 48.858610000000006,
-      longitude: 2.330499,
+      latitude: 52.3609553943508,
+      longitude: 4.85309666406198,
       zoom: 16,
     },
     isFavorite: false,
@@ -54,16 +54,16 @@ export const offers: Offer[] = [
     price: 806,
     previewImage: 'https://15.design.htmlacademy.pro/static/hotel/3.jpg',
     city: {
-      name: 'Paris',
+      name: 'Amsterdam',
       location: {
-        latitude: 48.85661,
-        longitude: 2.351499,
+        latitude: 52.3609553943508,
+        longitude: 4.85309666406198,
         zoom: 13,
       },
     },
     location: {
-      latitude: 48.834610000000005,
-      longitude: 2.335499,
+      latitude: 52.3909553943508,
+      longitude: 4.929309666406198,
       zoom: 16,
     },
     isFavorite: false,
@@ -77,16 +77,16 @@ export const offers: Offer[] = [
     price: 102,
     previewImage: 'https://15.design.htmlacademy.pro/static/hotel/11.jpg',
     city: {
-      name: 'Paris',
+      name: 'Amsterdam',
       location: {
-        latitude: 48.85661,
-        longitude: 2.351499,
+        latitude: 52.3609553943508,
+        longitude: 4.85309666406198,
         zoom: 13,
       },
     },
     location: {
-      latitude: 48.85761,
-      longitude: 2.358499,
+      latitude: 52.3809553943508,
+      longitude: 4.939309666406198,
       zoom: 16,
     },
     isFavorite: false,
@@ -102,8 +102,8 @@ export const offers: Offer[] = [
     city: {
       name: 'Paris',
       location: {
-        latitude: 48.85661,
-        longitude: 2.351499,
+        latitude: 52.3609553943508,
+        longitude: 4.85309666406198,
         zoom: 13,
       },
     },
--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,37 +1,57 @@
 import Header from '@/components/header/header'
 import Tabs from '@/components/tabs/tabs'
 import Sort from '@/components/sort/sort'
-import { Offer } from '@/types/offers'
+import { Offer, OfferCity } from '@/types/offers'
 import OffersList from '@/components/offers-list/offers-list'
 import Map from '@/components/map/map'
+import { useState } from 'react'
 
 type MainProps = {
   offers: Offer[]
 }
 
-const Main = ({ offers }: MainProps): JSX.Element => (
-  <div className="page page--gray page--main">
-    <Header />
-    <main className="page__main page__main--index">
-      <h1 className="visually-hidden">Cities</h1>
-      <Tabs />
-      <div className="cities">
-        <div className="cities__places-container container">
-          <section className="cities__places places">
-            <h2 className="visually-hidden">Places</h2>
-            <b className="places__found">312 places to stay in Amsterdam</b>
-            <Sort />
-            <div className="cities__places-list places__list tabs__content">
-              <OffersList listOffers={offers} />
+const Main = ({ offers }: MainProps): JSX.Element => {
+  const [selectedOfferId, setSelectedOfferId] = useState<string | undefined>(
+    undefined,
+  )
+  const city = offers.find((offer) => offer.city.name === 'Amsterdam')?.city
+
+  const handleOfferListHover = (listOfferItemId: string) => {
+    setSelectedOfferId(listOfferItemId)
+  }
+
+  return (
+    <div className="page page--gray page--main">
+      <Header />
+      <main className="page__main page__main--index">
+        <h1 className="visually-hidden">Cities</h1>
+        <Tabs />
+        <div className="cities">
+          <div className="cities__places-container container">
+            <section className="cities__places places">
+              <h2 className="visually-hidden">Places</h2>
+              <b className="places__found">312 places to stay in Amsterdam</b>
+              <Sort />
+              <div className="cities__places-list places__list tabs__content">
+                <OffersList
+                  listOffers={offers}
+                  onMouseEnter={handleOfferListHover}
+                />
+              </div>
+            </section>
+            <div className="cities__right-section">
+              <Map
+                className={'cities__map'}
+                city={city as OfferCity}
+                offers={offers}
+                activeOfferId={selectedOfferId}
+              />
             </div>
-          </section>
-          <div className="cities__right-section">
-            <Map className="cities__map map" />
           </div>
         </div>
-      </div>
-    </main>
-  </div>
-)
+      </main>
+    </div>
+  )
+}
 
 export default Main
