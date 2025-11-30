3bf32ae68f3b4cd6f06d0ee7c7107fbacf94bae6
Vladimir <vlad.stavros@bk.ru>
2025-11-30 09:13:18 +0300

    Исправление ошибки

--- src/services/process-error-handle.ts
+++ src/services/process-error-handle.ts
@@ -3,7 +3,6 @@ import { clearErrorAction } from '@/store/api-actions'
 import { setError } from '@/store/auth/auth-slice'
 
 export const processErrorHandle = (message: string) => {
-  console.log(message)
   store.dispatch(setError(message))
   store.dispatch(clearErrorAction())
 }
